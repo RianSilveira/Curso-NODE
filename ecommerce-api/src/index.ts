5ee02dec501c4280ccfb364db891c00f35412d09
Ryan Silveira <ryansilveira577@gmail.com>
2026-02-24 23:51:25 -0300

    dia 02

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,11 +1,70 @@
-import express from "express";
+import express, { Request, Response, Application } from "express";
 
-const app = express();
 
-app.get("/", (req, res) => {
-    res.send("teste123");
+const app: Application = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+// Middleware para permitir que o Express entenda JSON
+app.use(express.json());
+
+// Rota principal
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("teste123");
 });
 
-app.listen(3000, () => {
-    console.log("servidor rodando na porta 3000");
+app.get("/products", async (req: Request, res: Response)=> 
+{
+    const Response = await fetch("https://dummyjson.com/products");
+
+    if (!Response.ok){
+        throw new Error("erro na requisiçao")
+    }
+    const data = await Response.json();
+    
+
+    res.json(data);
+    
+});
+let id = 1;
+let usuarios: {id: number, nome: string, email: string}[] = [];
+
+app.post("/Users", (req: Request, res: Response)=> {
+   
+    let user = req.body;
+    user.id = id ++;
+    usuarios.push(user);
+    res.send({
+        message:"Usuario Criado com Sucesso"
+    });
+});
+
+app.get("/Users/:id", (req: Request, res: Response)=> {
+    
+   let userId = Number(req.params.id)
+   let user = usuarios.find(user => user?.id === userId)
+
+   if (!user){
+    return res.status(404).json(
+        {
+            message: "Usuário não encontrado"
+        }
+    );
+   }
+
+   return res.send({user})
+});
+
+
+
+app.get("/Users", (req: Request, res: Response)=> {
+    
+    res.send({
+       usuarios
+    });
+});
+
+// Inicialização do servidor
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
